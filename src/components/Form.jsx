af4a47182de6ceac355c27f5258d4fd43f85156c
Retro-Markz <cubedomarco@gmail.com>
2024-09-22 07:08:23 -0700

    form functionality start

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,47 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmimt(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="flex p-8 items-center justify-center flex-col md:flex-row bg-amber-600 gap-2">
+    <form
+      className="flex p-8 items-center justify-center flex-col md:flex-row bg-amber-600 gap-2"
+      onSubmit={handleSubmimt}
+    >
       <h3 className="text-lg font-semibold">What do you need for your trip?</h3>
-      <input className="input" type="text" />
-      <input className="input" type="text" placeholder="Item" />
+      <select
+        className="input px-4 py-2 text-lg"
+        onChange={(e) => setQuantity(Number(e.target.value))}
+        value={quantity}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        className="input"
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button className="secondary-cta">Add</button>
-    </div>
+    </form>
   );
 };
 
--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -26,7 +26,7 @@ const PackingList = () => {
     <div className="flex flex-col justify-center items-center bg-amber-950 w-screen h-[50vh] gap-12">
       <ul className="list *:text-amber-200 *:text-2xl *:font-semibold gap-4">
         {items.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
 
