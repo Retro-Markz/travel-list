47ba16f7b8450ae06bfefa42aee6ec7b51368012
MARCO CUBEDO <115151977+Retro-Markz@users.noreply.github.com>
2024-10-09 03:19:01 -0700

    status update and done

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -7,12 +7,16 @@ const Stats = ({ items }) => {
   return (
     <div className="flex justify-center w-[100vw]">
       <footer className="bg-blue-500 w-full flex font-semibold text-2xl justify-center items-center p-8 gap-4">
-        <em>
-          {percentage === 100
-            ? "you got everything packed ✈️"
-            : `🧳 You have ${numItems} items on your list, {numPacked} (
+        {numItems === 0 ? (
+          <em>Start Adding some items 🚀</em>
+        ) : (
+          <em>
+            {percentage === 100
+              ? "you got everything packed ✈️"
+              : `🧳 You have ${numItems} items on your list, {numPacked} (
           ${numItems === 0 ? "0" : percentage}%) is already packed!`}
-        </em>
+          </em>
+        )}
       </footer>
     </div>
   );
